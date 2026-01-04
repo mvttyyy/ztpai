3f89e337675fa32fe059807e3cae7bd884a8b579
mvttyyy <mvvtty@gmail.com>
2026-01-04 15:26:10 +0100

    feat(backend): add optional JWT guard for public routes

---
+++ backend/src/modules/auth/guards/optional-jwt-auth.guard.ts
@@ -0,0 +1,14 @@
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class OptionalJwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context);
+  }
+
+  handleRequest(err: any, user: any) {
+    // Don't throw an error if no user
+    return user || null;
+  }
+}
