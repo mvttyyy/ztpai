da37395b29163d442548333025f2e22ea84a77f3
mvttyyy <mvvtty@gmail.com>
2026-01-05 17:09:48 +0100

    feat(backend): add user update DTO

---
+++ backend/src/modules/users/dto/update-user.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsOptional, MaxLength, MinLength } from 'class-validator';
+
+export class UpdateUserDto {
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(30)
+  username?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  @MaxLength(500)
+  bio?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  avatarUrl?: string;
+}
